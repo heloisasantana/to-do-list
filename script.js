a0a0c645b22eee3ab9e48f74ddcbffc338cc2746
heloisasantana <heloisasantana.silva@hotmail.com>
2022-04-29 17:26:16 -0300

    Conclusão do Requisito 7

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
+// Requisito 5 e 6
 const botaoAdicionar = document.getElementById("criar-tarefa");
 botaoAdicionar.addEventListener("click", adicionaTarefa);
 
 function adicionaTarefa() {
 const listaOrdenada = document.getElementById("lista-tarefas");
 const inputTexto = document.getElementById("texto-tarefa");
-const itemLista = document.createElement("li");
-itemLista.innerText = inputTexto.value;
-listaOrdenada.appendChild(itemLista);
+const itemListaCriado = document.createElement("li");
+itemListaCriado.addEventListener("click", corDeFundo);
+itemListaCriado.innerText = inputTexto.value;
+listaOrdenada.appendChild(itemListaCriado);
+itemListaCriado.className = "item-lista";
 inputTexto.value = "";
 }
+
+// Requisito 7
+function corDeFundo(event){
+event.target.classList.add("item-selecionado");
+}
+
 
