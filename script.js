6c8b112138e4a1d51dfdc2030bf95f2a4b09b258
heloisasantana <heloisasantana.silva@hotmail.com>
2022-04-29 19:13:45 -0300

    Conclusão do Requisito 9

--- script.js
+++ script.js
@@ -7,19 +7,27 @@ const listaOrdenada = document.getElementById("lista-tarefas");
 const inputTexto = document.getElementById("texto-tarefa");
 const itemListaCriado = document.createElement("li");
 itemListaCriado.addEventListener("click", corDeFundo);
+itemListaCriado.addEventListener("dblclick", addOrRemoveCompleted);
 itemListaCriado.innerText = inputTexto.value;
 listaOrdenada.appendChild(itemListaCriado);
 itemListaCriado.className = "item-lista";
 inputTexto.value = "";
 }
 
-// Requisito 7
+// Requisito 7 e 8
 function corDeFundo(event){
 const itemLista = document.getElementsByTagName("li");
 for (let index = 0; index < itemLista.length; index += 1)
 {itemLista[index].classList.remove("item-selecionado")}
 event.target.classList.add("item-selecionado")
 }
+
+// Requisito 9
+function addOrRemoveCompleted(event){
+const itemDuploClick = event.target.classList;
+itemDuploClick.toggle("completed");
+}
+
 
 
 
