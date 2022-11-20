6f17cd90d56664d1f812e0d76a9b6d40ae3249b3
heloisasantana <heloisasantana.silva@hotmail.com>
2022-11-20 20:10:18 -0300

    Estilização

--- script.js
+++ script.js
@@ -1,36 +1,34 @@
-// Requisito 5 e 6
-const botaoAdicionar = document.getElementById("criar-tarefa");
-botaoAdicionar.addEventListener("click", adicionaTarefa);
+function addOrRemoveCompleted(event) {
+  event.target.classList.toggle('completed');
+}
+
+function corDeFundo(event) {
+  const itemLista = document.getElementsByTagName('li');
+  for (let index = 0; index < itemLista.length; index += 1) {
+    itemLista[index].classList.remove('item-selecionado');
+  }
+  event.target.classList.add('item-selecionado');
+}
 
 function adicionaTarefa() {
-const listaOrdenada = document.getElementById("lista-tarefas");
-const inputTexto = document.getElementById("texto-tarefa");
-const itemListaCriado = document.createElement("li");
-itemListaCriado.addEventListener("click", corDeFundo);
-itemListaCriado.addEventListener("dblclick", addOrRemoveCompleted);
-itemListaCriado.innerText = inputTexto.value;
-listaOrdenada.appendChild(itemListaCriado);
-itemListaCriado.className = "item-lista";
-inputTexto.value = "";
+  const listaOrdenada = document.getElementById('lista-tarefas');
+  const inputTexto = document.getElementById('texto-tarefa');
+  const itemListaCriado = document.createElement('li');
+  itemListaCriado.addEventListener('click', corDeFundo);
+  itemListaCriado.addEventListener('dblclick', addOrRemoveCompleted);
+  itemListaCriado.innerText = inputTexto.value;
+  listaOrdenada.appendChild(itemListaCriado);
+  itemListaCriado.className = 'item-lista';
+  inputTexto.value = '';
 }
 
-// Requisito 7 e 8
-function corDeFundo(event){
-const itemLista = document.getElementsByTagName("li");
-for (let index = 0; index < itemLista.length; index += 1)
-{itemLista[index].classList.remove("item-selecionado")}
-event.target.classList.add("item-selecionado")
+const botaoAdicionar = document.getElementById('criar-tarefa');
+botaoAdicionar.addEventListener('click', adicionaTarefa);
+
+function apagaTudo() {
+  const listaOrdenada = document.getElementById('lista-tarefas');
+  listaOrdenada.innerHTML = '';
 }
 
-// Requisito 9
-function addOrRemoveCompleted(event){
-const itemDuploClick = event.target.classList;
-itemDuploClick.toggle("completed");
-}
-
-
-
-
-
-
-
+const botaoApagar = document.getElementById('apaga-tudo');
+botaoApagar.addEventListener('click', apagaTudo);
